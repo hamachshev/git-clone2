1e09cc057b3c7b7d2e3434b2029dcbc8fb8198f4
Aharon Seidman <aharondov1@gmail.com>
2025-02-04 23:15:54 -0500

    added shortest unique hash func to cat-file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use flate2::read::ZlibDecoder;
 use flate2::write::ZlibEncoder;
 use flate2::Compression;
 use std::{
+    ffi::CStr,
     fs,
-    io::{BufReader, Read, Write},
+    io::{BufRead, BufReader, Read, Write},
     path::PathBuf,
 };
 
 use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
 
-use hex_literal::hex;
 use sha1::{Digest, Sha1};
 
 #[derive(Parser)]
@@ -32,6 +33,12 @@ enum Commands {
         stdin: Option<bool>,
 
         file: Option<PathBuf>,
+    },
+    CatFile {
+        #[arg(short)]
+        pretty_print: bool,
+
+        hash: String,
     },
 }
 
@@ -87,6 +94,56 @@ fn main() -> Result<()> {
 
                     println!("{:x}", result)
                 }
+            }
+        }
+        Commands::CatFile { pretty_print, hash } => {
+            anyhow::ensure!(pretty_print, "must have pretty print for now");
+
+            let mut path_iter = fs::read_dir(format!(".git/objects/{}", &hash[..2]))?
+                .into_iter()
+                .filter(|e| {
+                    // get back result from the read_dir so have to unwrap
+                    let Ok(e) = e else {
+                        return false;
+                    };
+                    let file_name = e.file_name();
+                    //have to convert to str to use starts with, ok bc must be valid utf-8 for git
+                    //anyway
+                    let Some(file_name) = file_name.to_str() else {
+                        return false;
+                    };
+                    file_name.starts_with(&hash[2..])
+                });
+
+            // double ?? because read_dir returns a result, and .next() returns an option so you get
+            // Result<Option<>>
+            let path = path_iter.next().context("error in getting path")??;
+            anyhow::ensure!(path_iter.next().is_none(), "not a unique hash");
+
+            let file =
+                fs::File::open(path.path()).context("opening the file to read the contents")?;
+            let z = ZlibDecoder::new(file);
+            let mut buf_read = BufReader::new(z);
+            let mut header = Vec::new();
+            buf_read.read_until(0u8, &mut header);
+            let header = CStr::from_bytes_with_nul(&header).expect("must be a nul byte at the end");
+            let header = header.to_str().context("header must be valid utf-8")?;
+            println!("{}", header);
+            let Some((blob_type, size)) = header.split_once(" ") else {
+                anyhow::bail!("wrong format of file");
+            };
+
+            match blob_type.trim() {
+                "blob" => {
+                    let size: u64 = size.parse().context("invalid size")?;
+                    let mut handle = buf_read.take(size);
+                    let mut buffer = Vec::new();
+                    handle.read_to_end(&mut buffer)?;
+                    let contents = String::from_utf8_lossy(&buffer);
+
+                    print!("contents:{}", &contents);
+                }
+                _ => anyhow::bail!("unknown file type"),
             }
         }
     }
