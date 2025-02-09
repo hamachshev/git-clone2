da0c9fc1cd25784ac7d785cbe15508aa90445613
Aharon Seidman <aharondov1@gmail.com>
2025-02-08 23:48:34 -0500

    refactored a bit and added some color to terminal entries

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -1,11 +1,11 @@
+use anyhow::{Context, Result};
+use colored::Colorize;
 use std::{
     collections::HashSet,
     fs,
     io::{BufReader, Read},
-    path::{Path, PathBuf},
+    path::Path,
 };
-
-use anyhow::{Context, Result};
 
 use crate::objects::{
     commit::Commit,
@@ -15,6 +15,81 @@ use crate::objects::{
 };
 
 pub fn invoke() -> Result<()> {
+    let files_in_commit = get_files_in_commit()?;
+    let mut modified_files = Vec::new();
+    let mut staged_files = Vec::new();
+
+    let index_file = IndexFile::read_from_index()?;
+
+    for entry in index_file.entries.iter() {
+        let other = IndexEntry::from_path(entry.entry_path.clone(), &entry.hash, entry.flags)
+            .context("constructing index entry from path")?;
+        if other != *entry {
+            modified_files.push(entry.entry_path.clone());
+        }
+
+        if !files_in_commit.contains(entry.hash.as_str()) {
+            //ie if files_in_commit does not contain one of the index files, it is new, ie staged
+            //for commit
+            staged_files.push(entry.entry_path.clone());
+        }
+    }
+
+    let index_hash_set: HashSet<String> = index_file
+        .entries
+        .into_iter()
+        .filter_map(|e| {
+            let path_str = e.entry_path.to_str()?;
+            path_str.parse::<String>().ok()
+        })
+        .collect();
+
+    let mut unstaged = Vec::new();
+    check_dir_for_unstaged(Path::new("."), &index_hash_set, &mut unstaged)?;
+
+    println!("modified----------------------------------");
+    for file in modified_files {
+        println!("{:#?}", file);
+    }
+    println!("staged-------------------------------------");
+    for file in staged_files {
+        println!("{:#?}", file);
+    }
+    println!("unstaged-------------------------------------");
+    for file in unstaged {
+        println!("{}", file.red());
+    }
+
+    Ok(())
+}
+
+fn check_dir_for_unstaged(
+    dir: &Path,
+    index_hash_set: &HashSet<String>,
+    mut unstaged: &mut Vec<String>,
+) -> Result<()> {
+    anyhow::ensure!(dir.is_dir(), "must pass dir for this to work");
+
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() && !(path.starts_with("./target") || path.starts_with("./.git")) {
+            // workaround
+            // for the fact that no .gitignore support yet
+            check_dir_for_unstaged(&path, &index_hash_set, &mut unstaged)?;
+        } else {
+            let path_trunc = path.to_str().context("unlikely error in path trunc")?;
+            if !index_hash_set.contains(&path_trunc[2..])
+                && !(path.starts_with("./target") || path.starts_with("./.git"))
+            {
+                unstaged.push(path_trunc[2..].to_string());
+            }
+        }
+    }
+    Ok(())
+}
+
+fn get_head_hash() -> Result<String> {
     let mut head_file = fs::File::open(".git/HEAD").context("opening HEAD file")?;
     let mut head_ref = String::new();
 
@@ -38,7 +113,11 @@ pub fn invoke() -> Result<()> {
         // we are in detached state where the HEAD file stores a hash
         head_ref.trim().to_string()
     };
+    Ok(head_hash)
+}
 
+fn get_files_in_commit() -> Result<HashSet<String>> {
+    let head_hash = get_head_hash()?;
     let commit = Commit::read_from_hash(&head_hash)?;
 
     let mut files_in_commit = HashSet::new();
@@ -49,72 +128,5 @@ pub fn invoke() -> Result<()> {
     let tree = Tree::read(&mut bufread).context("reading tree from tree object from commit")?;
     tree.traverse(&mut files_in_commit)
         .context("getting files_in_commit of blobs in commit")?;
-
-    let mut modified_files = Vec::new();
-    let mut staged_files = Vec::new();
-
-    let index_file = IndexFile::read_from_index()?;
-
-    for entry in index_file.entries.iter() {
-        let other = IndexEntry::from_path(entry.entry_path.clone(), &entry.hash, entry.flags)
-            .context("constructing index entry from path")?;
-        if other != *entry {
-            modified_files.push(entry.entry_path.clone());
-        }
-
-        if !files_in_commit.contains(entry.hash.as_str()) {
-            //ie if files_in_commit does not contain one of the index files, it is new, ie staged
-            //for commit
-            staged_files.push(entry.entry_path.clone());
-        }
-    }
-
-    let index_hash_set: HashSet<String> = index_file
-        .entries
-        .into_iter()
-        .map(|e| {
-            let path_str: String = e.entry_path.clone().to_str().unwrap().parse().unwrap();
-            path_str
-        })
-        .collect();
-
-    let mut unstaged = Vec::new();
-    check_dir_for_unstaged(Path::new("."), &index_hash_set, &mut unstaged)?;
-
-    println!("modified----------------------------------");
-    for file in modified_files {
-        println!("{:#?}", file);
-    }
-    println!("staged-------------------------------------");
-    for file in staged_files {
-        println!("{:#?}", file);
-    }
-    println!("unstaged-------------------------------------");
-    for file in unstaged {
-        println!("{:#?}", file);
-    }
-
-    Ok(())
-}
-
-fn check_dir_for_unstaged(
-    dir: &Path,
-    index_hash_set: &HashSet<String>,
-    mut unstaged: &mut Vec<PathBuf>,
-) -> Result<()> {
-    anyhow::ensure!(dir.is_dir(), "must pass dir for this to work");
-
-    for entry in fs::read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_dir() && !(path.starts_with("./target") || path.starts_with("./.git")) {
-            check_dir_for_unstaged(&path, &index_hash_set, &mut unstaged)?;
-        } else {
-            let path_trunc = path.to_str().context("unlikely error in path trunc")?;
-            if !index_hash_set.contains(&path_trunc[2..]) {
-                unstaged.push(path);
-            }
-        }
-    }
-    Ok(())
+    Ok(files_in_commit)
 }
